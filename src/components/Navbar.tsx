daa1cb8f217cb849a92a30c0b188b4ced1188d37
Mark Baydoun <markosbo95@gmail.com>
2020-09-06 22:16:00 +0200

    made mobile responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ const Navbar = ({ setShow }: any) => {
 
   return (
     <nav className='nav'>
-      <div>
+      <div className='nav--1'>
         <div className="nav__social">
           <a href="#" className="btn nav__btn" onClick={() => setShow(true)}>Contact</a>
           <a href="https://github.com/markbayor" target="_blank" rel="noopener noreferrer" className="a__link contact__content-link"><GithubIcon className='icon icon__small' /></a>
           <a href="https://www.linkedin.com/in/markbayor/" target="_blank" rel="noopener noreferrer" className="a__link contact__content-link"><LinkedinIconSmall className='icon icon__small' /></a>
         </div>
       </div>
-      <div>
+      <div className='nav--2'>
         <a href="#introduction" className="btn nav__btn">Introduction</a>
         <a href="#projects" className="btn nav__btn">Projects</a>
         <a href="#technologies" className="btn nav__btn">Technologies</a>
